1a721df68dd67c60f0aecca2cbd6984d0ac80004
BelChaparro <a.belenchaparro@gmail.com>
2024-09-30 20:46:21 -0300

    Included comment with obervation of what I expect that the function does

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,5 +1,7 @@
-﻿public static class MysteryStack1 {
-    public static string Run(string text) {
+﻿public static class MysteryStack1
+{
+    public static string Run(string text)
+    {
         var stack = new Stack<char>();
         foreach (var letter in text)
             stack.Push(letter);
@@ -11,3 +13,6 @@
         return result;
     }
 }
+// Pushes all letters in a text inside the stack.
+// Then pops a letter from the stack and adds them to a string variable until the stack is empty.
+// The resulting string is the original text inversed.
